cd8da1375aa7d362a3592b4dacfe216de8d4fbcb
Berk Cohadar <berkcohadar1@gmail.com>
2021-10-13 11:14:07 -0400

    Game moved to spriteKit. Game scene added. No funcs.

---
+++ Ping Pong/Ping PongTests/Ping_PongTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ping_PongTests.swift
+//  Ping PongTests
+//
+//  Created by Berk Çohadar on 10/13/21.
+//
+
+import XCTest
+@testable import Ping_Pong
+
+class Ping_PongTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
